24d4fb28195b403ccf5919bbcbc703496bd8e2ef
Ammar Araby <ammararbie424@gmail.com>
2024-08-08 19:32:33 +0300

    email validation

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,19 @@
-// document
-//   .getElementById("emailForm")
-//   .addEventListener("submit", function (event) {
-//     event.preventDefault(); // Prevent the form from submitting
-
-//     const emailInput = document.getElementById("email");
-//     const email = emailInput.value;
-//     const errorMessage = document.getElementById("error-message");
-
-//     // Regex pattern for email validation
-//     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-//     // Validate the email
-//     if (emailPattern.test(email)) {
-//       errorMessage.textContent = ""; // Clear any previous error message
-//       // Perform form submission or other actions here
-//     } else {
-//       errorMessage.textContent = "Invalid email address";
-//       errorMessage.style.display = "block";
-//     }
-//   });
+document
+  .getElementById("emailForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+  });
+let email = document.getElementById("email");
+const errorMessage = document.getElementById("error-message");
+email.oninput = function () {
+  emailValue = email.value;
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (emailPattern.test(emailValue)) {
+    errorMessage.textContent = ""; // Clear any previous error message
+    // Perform form submission or other actions here
+  } else {
+    errorMessage.textContent = "Invalid email address";
+  }
+  // console.log(email.value);
+};
+// console.log(email);
